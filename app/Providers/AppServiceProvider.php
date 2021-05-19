dce531a7320b02538ea42910e3d832ebfc40e176
dr_mano <razausmankhan97@gmail.com>
2021-05-19 09:50:10 +0500

    Added ModelObserver and a Helper Class
    a generic model observer and added a Helper Class that can be used
    anywhere

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\EntityType;
 use App\Models\Page;
+use App\Models\Product;
+use App\Models\ProductCategory;
+use App\Models\ProductStock;
+use App\Models\Remark;
+use App\Models\User;
+use App\Observers\ModelObserver;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,6 +32,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // model observers
+
+        Page::observe(ModelObserver::class) ;
+        User::observe(ModelObserver::class) ;
+        ProductCategory::observe(ModelObserver::class);
+        Product::observe(ModelObserver::class);
+        ProductStock::observe(ModelObserver::class);
+        EntityType::observe(ModelObserver::class);
+        Remark::observe(ModelObserver::class);
+
          //partials._pages_menu
          View::composer('partials._pages_menu', function($view)
          {
